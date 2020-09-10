ef22b1f6b6f59d4a804855edc78c25c1e9ed539a
lucianodos <lucianodos.oliveira@gmail.com>
2020-09-10 14:44:12 -0300

    removendo umas var_dumps

--- ope.php
+++ ope.php
@@ -5,16 +5,13 @@ session_start();
 $login = $_POST['login'];
 $senha = $_POST['senha'];
 
-$sql = "select * from usuario where name =". $login . " and senha = " . $senha;
-
-var_dump($sql);
 $conn = new PDO('mysql:host=localhost;dbname=server', 'root', 1234) or die("erro na conexao!");
 $stmt = $conn->prepare("SELECT * FROM usuario WHERE nome = :login AND senha = :senha");
 $stmt->execute(array('login' => $login, 'senha' => $senha));
-$nome = $conn->query($sql);
-var_dump($nome);
+
+
 $result = $stmt->fetchAll();
-// var_dump($conn);
+
 if(count($result)) {
     $_SESSION['login'] = $login;
     $_SESSION['senha'] = $senha;
